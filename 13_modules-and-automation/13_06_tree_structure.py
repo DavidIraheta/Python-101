99778656a29f5afebac73f54531d4a64620c9c70
David Iraheta <david.iraheta.89@gmail.com>
2024-10-20 11:26:58 -0600

    Python101_10.20

--- 13_modules-and-automation/13_06_tree_structure.py
+++ 13_modules-and-automation/13_06_tree_structure.py
@@ -2,6 +2,13 @@
 # and prints out all the Python files it can find.
 # Run it in your labs folder and add formatting for nicer viewing.
 from pathlib import Path
-import os
-labs = Path.home().joinpath("Users"")
+labs_folder = Path("/Users/davidiraheta/Documents/CodingNomads/labs")
+for root, dirs, files in labs_folder.rglob("*.py"):
+    for file in files:
+        if file.suffix == ".py":
+            print(file)
+
+#
+
+                            
 
--- projects/hangman.py
+++ projects/hangman.py
@@ -13,43 +13,127 @@
 #   filled in, e.g.: "_ e _ _ _" if they guessed "e" from "hello"
 # Display a winning message and the full word if they win
 # Display a losing message and quit the game if they don't make it
-
-```word_to_guess = "halloween"
+word_to_guess = "halloween"
 word_length = len(word_to_guess)
 word_display = ["_"] * word_length
-remaining_attempts = 5
+remaining_attempts = 6
 guessed_letters = set()
 print("Welcome to Hangman!")
-print("Try to guess the word")
+print("Try to guess the word:")
 print(" ".join(word_display))
+print(f"You have {remaining_attempts} attempts to guess the word.")
 while remaining_attempts > 0:
     user_guess = input("Enter a letter: ").lower()
     if len(user_guess) != 1 or not user_guess.isalpha():
-        print("Please enter a single letter")
+        print("Please enter a single alphabetic letter.")
         continue
     if user_guess in guessed_letters:
-        print(f"You have already guessed {user_guess}. Try a different letter")
-        print(" ".join(word_display))
+        print(f"You've already guessed '{user_guess}'. Try a different letter.")
         continue
     guessed_letters.add(user_guess)
     if user_guess in word_to_guess:
-        print(f"Great job! {user_guess} is in the word")
-        print(" ".join(word_display))
+        print(f"Great job! '{user_guess}' is in the word.")
         for i in range(word_length):
             if word_to_guess[i] == user_guess:
                 word_display[i] = user_guess
     else:
         remaining_attempts -= 1
-        print(f"Sorry, {user_guess} is not in the word")
-        print(f"You have {remaining_attempts} remaining attempts")
-        print(" ".join(word_display))
-        if "_" not in word_display:
-            print("Congratulations! You have won!")
-            break
-        if word_to_guess == "".join(word_display):
-            print("Congratulations! You have won!")
-            break
-        if remaining_attempts == 0:
-            print("Sorry, you have run out of attempts")
-            print(f"The word was {word_to_guess}")
-            break````
+        print(f"Sorry, '{user_guess}' is not in the word.")
+        print(f"You have {remaining_attempts} remaining attempts.")
+    print(" ".join(word_display))
+    if "_" not in word_display:
+        print("Congratulations! You've guessed the word!")
+        break
+    if remaining_attempts == 0:
+        print("Sorry, you've run out of attempts.")
+        print(f"The word was '{word_to_guess}'.")
+        break
+
+
+
+# word_to_guess = "halloween"
+# word_length = len(word_to_guess)
+# word_display = ["_"] * word_length
+# remaining_attempts = 8
+# guessed_letters = set()
+# print("Welcome to Hangman!")
+# print(" ".join(word_display))
+# print("Try to guess the word")
+# print(f"You have {remaining_attempts} attempts to guess the word")
+
+# while remaining_attempts > 0:
+#     user_guess = input("Enter a letter: ").lower()
+#     print(" ".join(word_display))
+#     if len(user_guess) != 1 or not user_guess.isalpha():
+#         print("Please enter a single letter")
+#         continue
+#     if user_guess in guessed_letters:
+#         # print(" ".join(word_display))
+#         print(f"You have already guessed {user_guess}. Try a different letter")
+#         print(" ".join(word_display))
+#         continue
+#     guessed_letters.add(user_guess)
+#     if user_guess in word_to_guess:
+#         print(" ".join(word_display))
+#         print(f"Great job! {user_guess} is in the word")
+#         continue
+#     for i in range(word_length):
+#             if word_to_guess[i] == user_guess:
+#                 word_display[i] = user_guess
+#     else:
+#         remaining_attempts -= 1
+#     print(" ".join(word_display))
+#     print(f"Sorry, {user_guess} is not in the word")
+#     print(f"You have {remaining_attempts} remaining attempts")
+#     if "_" not in word_display:
+#          print("Sorry, that letter is not in the word")
+#     break
+# if word_to_guess == "".join(word_display):
+#         print("Congratulations! You have won!")
+# if remaining_attempts == 0:
+#         print("Sorry, you have run out of attempts")
+#         print(f"The word was {word_to_guess}")
+
+# word_to_guess = "halloween"
+# word_length = len(word_to_guess)
+# word_display = ["_"] * word_length
+# remaining_attempts = 8
+# guessed_letters = set()
+# print("Welcome to Hangman!")
+# print(" ".join(word_display))
+# print("Try to guess the word")
+# print(f"You have {remaining_attempts} attempts to guess the word")
+
+# while remaining_attempts > 0:
+#     user_guess = input("Enter a letter: ").lower()
+#     print(" ".join(word_display))
+#     if len(user_guess) != 1 or not user_guess.isalpha():
+#         print("Please enter a single letter")
+#         continue
+#     if user_guess in guessed_letters:
+#         print(" ".join(word_display))
+#         print(f"You have already guessed {user_guess}. Try a different letter")
+#         continue
+#     guessed_letters.add(user_guess)
+#     if user_guess in word_to_guess:
+#         print(" ".join(word_display))
+#         print(f"Great job! {user_guess} is in the word")
+#         continue
+#     for i in range(word_length):
+#             if word_to_guess[i] == user_guess:
+#                 word_display[i] = user_guess
+#     else:
+#         remaining_attempts -= 1
+#     print(" ".join(word_display))
+#     print(f"Sorry, {user_guess} is not in the word")
+#     print(f"You have {remaining_attempts} remaining attempts")
+#     if "_" not in word_display:
+#          print("Sorry, that letter is not in the word")
+#     break
+#     if word_to_guess == "".join(word_display):
+#         print("Congratulations! You have won!")
+#     break
+#     if remaining_attempts == 0:
+#         print("Sorry, you have run out of attempts")
+#         print(f"The word was {word_to_guess}")
+#     break
